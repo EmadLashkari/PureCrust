546342194c89b00e6de5666686d020109e7f195f
Emad Lashkari <emadlashkari.82@gmail.com>
2025-09-22 12:36:18 +0330

    add smooth scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
+"use client";
 import Header from "@/components/sections/Header";
 import Hero from "@/components/sections/Hero";
 import Production from "@/components/sections/Production";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 export default function Home() {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      content: "#smooth-content",
+      wrapper: "#smooth-wrapper",
+      smooth: 3,
+      effects: true,
+    });
+  });
   return (
-    <main className="overflow-hidden">
-      <Header />
-      <Hero />
-      <Production />
+    <main id="smooth-wrapper" className="overflow-hidden">
+      <section id="smooth-content">
+        <Header />
+        <Hero />
+        <Production />
+      </section>
     </main>
   );
 }
