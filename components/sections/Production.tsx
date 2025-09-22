907fe45b091c2379d9ddbdd1401c4cee7a08f8b5
Emad Lashkari <emadlashkari.82@gmail.com>
2025-09-22 12:45:27 +0330

    change layout and add hover animation

--- components/sections/Production.tsx
+++ components/sections/Production.tsx
@@ -1,6 +1,6 @@
 "use client";
 import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
+import { ContextSafeFunc, useGSAP } from "@gsap/react";
 import { ScrollSmoother, ScrollTrigger, SplitText } from "gsap/all";
 import Image from "next/image";
 import { Inter } from "next/font/google";
@@ -14,33 +14,49 @@ gsap.registerPlugin(ScrollTrigger, SplitText, ScrollSmoother);
 export default function Production() {
   // const container = useRef(null);
 
-  useGSAP(() => {
+  const { contextSafe } = useGSAP(() => {
     const SplitedText = new SplitText(".prodText", {
       type: "lines",
-      mask:"lines",
+      mask: "lines",
+    });
+    gsap.set(".popup", {
+      opacity: 0,
+      autoAlpha: 0,
     });
 
     const productionTl = gsap.timeline({
       scrollTrigger: {
         trigger: ".prod-container",
         start: "top 80%",
-        end: "bottom bottom",
-        scrub : 3,
+        end: "center 60%",
+        scrub: 3,
         markers: true,
       },
     });
-    
+
     productionTl.from(SplitedText.lines, {
-      duration:5,
+      duration: 5,
       yPercent: 100,
       opacity: 0,
-        stagger: 2,
-        ease: "expo.inOut",
+      stagger: 2,
+      ease: "expo.inOut",
+    });
+  });
+
+  const popUpAnimation = contextSafe(() => {
+    gsap.to(".popup", {
+      opacity: 1,
+      autoAlpha: 1,
+    });
+  });
+  const inversePopupAnimation = contextSafe(() => {
+    gsap.to(".popup", {
+      opacity: 0,
     });
   });
   return (
     <section className="prod-container relative w-full  justify-around items-center overflow-hidden">
-      <article className="pt-24 px-5 bg-pinky ">
+      <article className="py-24 px-5 bg-pinky ">
         {/* <Image
         className="prod-img md:float-left  rounded-2xl w-full"
         id="prod-img"
@@ -49,8 +65,28 @@ export default function Production() {
         height={200}
         alt="bakery image"
       /> */}
-      <p className={`${InterFont.className} text-cake prodText text-4xl/tight lg:text-9xl/tight pt-5 text-center`}>Pure Crust is a charming bakery shop that delights customers with its artisanal approach to baking. Nestled in a cozy corner of the neighborhood, it offers a wide array of freshly baked goods, from crusty sourdough loaves to delicate pastries and mouthwatering cakes. </p>
+        <Image
+          className="popup absolute left-1/6 top-0 opacity-0 z-50"
+          src={"/img/heather-barnes.jpg"}
+          alt="bakery"
+          width={300}
+          height={600}
+        />
+        <div
+          className={`${InterFont.className} text-cake prodText text-4xl/tight lg:text-5xl/tight py-5 text-center flex flex-col gap-20`}
+        >
+          <div
+            onMouseEnter={popUpAnimation}
+            onMouseLeave={inversePopupAnimation}
+          >
+            Bake-Off Bonanza
+          </div>
+          <div>Sweet Sensations</div>
+          <div>Taste of the Oven</div>
+          <div>Baked Bliss</div>
+          <div>Cookie Connoisseur Challenge</div>
+        </div>
       </article>
-      </section>
+    </section>
   );
 }
